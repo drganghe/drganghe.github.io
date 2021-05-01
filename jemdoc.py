1bada978dace9b496ba0480ad7d4182b0b6fe3a5
ganghe <steel.he@gmail.com>
2021-04-30 21:48:44 -0400

    index

--- jemdoc.py
+++ jemdoc.py
@@ -138,6 +138,7 @@ def standardconf():
   <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
   <head>
   <meta name="generator" content="jemdoc, see http://jemdoc.jaboc.net/" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
   
   [defaultcss]
